b67d77b15ae7942fe7fb95c631965ee6f48d38e0
Vitali Sidukov <vitaly.sidukov@nordigy.ru>
2020-07-08 22:28:12 +0300

    Add test testGetClassString

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,6 @@
 public class MainClass {
     private final int class_number = 20;
+    private final String class_string = "Hello, world";
 
     public int getLocalNumber() {
         return 14;
@@ -7,5 +8,9 @@ public class MainClass {
 
     public int getClassNumber() {
         return this.class_number;
+    }
+
+    public String getClassString() {
+        return this.class_string;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,21 +1,32 @@
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class MainClassTest {
     MainClass main = new MainClass();
-    int expected;
+    int expected_int;
+    String expected_hello;
+    String expected_Hello;
 
     @Test
     public void testGetLocalNumber() {
-        expected = 14;
-        assertEquals(String.format("GetLocalNumber return %s, not %s.", main.getLocalNumber(), expected), main.getLocalNumber(), expected);
+        expected_int = 14;
+        assertEquals(String.format("GetLocalNumber return %s, not %s.", main.getLocalNumber(), expected_int), main.getLocalNumber(), expected_int);
     }
 
     @Test
     public void testGetClassNumber() {
-        expected = 45;
-        assertTrue(String.format("GetClassNumber return less that %s.", expected), main.getClassNumber() > expected);
+        expected_int = 45;
+        assertTrue(String.format("GetClassNumber return less than %s.", expected_int), main.getClassNumber() > expected_int);
+    }
+
+    @Test
+    public void testGetClassString() {
+        expected_hello = "hello";
+        expected_Hello = "Hello";
+        assertThat(String.format("GetClassString don't have '%s' or '%s'.", expected_hello, expected_Hello),
+                main.getClassString().contains(expected_hello) || main.getClassString().contains(expected_Hello));
     }
 }
