85eaa0536899b714bc67fd31f30e358cea0c8e74
Vitali Sidukov <vitaly.sidukov@nordigy.ru>
2020-07-08 22:02:56 +0300

    Add test testGetClassNumber

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,12 +1,21 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MainClassTest {
     MainClass main = new MainClass();
+    int expected;
 
     @Test
     public void testGetLocalNumber() {
-        assertEquals("Incorrect return from getLocalNumber function.", main.getLocalNumber(), 14);
+        expected = 14;
+        assertEquals(String.format("GetLocalNumber return %s, not %s.", main.getLocalNumber(), expected), main.getLocalNumber(), expected);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        expected = 45;
+        assertTrue(String.format("GetClassNumber return less that %s.", expected), main.getClassNumber() > expected);
     }
 }
