264a7b45030ab6174c17878c02aac216f8011b12
Vitali Sidukov <vitaly.sidukov@nordigy.ru>
2020-09-02 00:59:26 +0300

    Add libs. Add Platform

--- src/tests/GetStartedTest.java
+++ src/tests/GetStartedTest.java
@@ -1,4 +1,4 @@
-package tests.iOS;
+package tests;
 
 import lib.CoreTestCase;
 import lib.ui.WelcomePageObject;
@@ -7,6 +7,9 @@ import org.junit.Test;
 public class GetStartedTest extends CoreTestCase {
     @Test
     public void testPassThoughtWelcome() {
+        if (this.platform.isAndroid()) {
+            return;
+        }
         WelcomePageObject welcomePageObject = new WelcomePageObject(driver);
         welcomePageObject.waitForLearnMoreLink();
         welcomePageObject.clickNextButton();
